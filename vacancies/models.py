cb2227b925e61a172578d630f50c55b72bf1cab0
ira <bendrikova.irina37@gmail.com>
2023-05-14 20:04:38 +0300

    Initial commit

---
+++ vacancies/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+
+class Vacancy(models.Model):
+    STATUS = [
+        ("draft", "Черновик"),
+        ("open", "Открыта"),
+        ("closed", "Закрыта")
+    ]
+
+    slug = models.SlugField(max_length=50)
+    text = models.CharField(max_length=2000)
+    status = models.CharField(max_length=6, choices=STATUS, default="draft")
+    created = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.slug
